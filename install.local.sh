8d0f2c892120a6775a117a3ed2dc223c45652f38
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-14 15:47:42 +0000

    Added customization script

---
+++ install.local.sh
@@ -0,0 +1,17 @@
+
+logmsg "Switching theme ..."
+wpcli 'theme activate wpcr-website-theme-2016"
+# Set permalinks and flush rewrite rules
+failing $? "Could not activate theme"
+
+logmsg "Setting permalink structure"
+wpcli "rewrite structure '/%postname%/' --hard"
+failing $? "Could change rewrite rules"
+
+logmsg "Flushing rewrite rules"
+wpcli "rewrite flush --hard"
+failing $? "Could flush rewrite rules"
+# Activate plugins
+logmsg "Activating plugins"
+wpcli "plugin activate --all"
+failing $? "Could not activate all plugins"
